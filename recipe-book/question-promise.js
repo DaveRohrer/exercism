6867e91ede873df905a3d9bb4eaf7f8e28cc7682
david <daverohrer@gmail.com>
2021-02-13 13:31:10 -0600

    first commit

---
+++ recipe-book/question-promise.js
@@ -0,0 +1,7 @@
+const questionPromise = (rl, question) => new Promise((resolve) => {
+  rl.question(question, (response) => {
+      resolve(response);
+  })
+});
+
+module.exports = questionPromise;
