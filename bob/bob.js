338b94cc09c6965637023ad4e9685e0869b37dcc
david <daverohrer@gmail.com>
2021-02-14 13:54:10 -0600

    Fixed parentheses problem

---
+++ bob/bob.js
@@ -0,0 +1,32 @@
+//
+// This is only a SKELETON file for the 'Bob' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const hey = (message) => {
+  
+  let response = 'Whatever.'
+
+  const allCaps = message === message.toUpperCase();
+  const question = message.includes('?');
+  const whiteSpaceOnly = 
+  
+
+  /*
+  if(all caps){
+    response = 'Whoa, chill out!;
+  }else if(question){
+    if(allcapsquestion){
+      response = 'Calm down, I know what I'm doing!';
+    }else{
+      response = 'Sure.';
+    }else if
+  }else if(whitespace only){
+    response = 'Fine. Be that way!'
+  }
+  //anything else response should be whatever
+  */
+
+  
+  return response;
+};
