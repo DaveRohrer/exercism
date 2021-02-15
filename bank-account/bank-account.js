118ec617d7a24d1cbcc4f33cf33baff0cd184401
david <daverohrer@gmail.com>
2021-02-14 19:16:58 -0600

    2/14 exercises.

--- bank-account/bank-account.js
+++ bank-account/bank-account.js
@@ -1,7 +1,7 @@
 //
 // This is only a SKELETON file for the 'Bank Account' exercise. It's been provided as a
 // convenience to get you started writing code faster.
-//
+//666
 
 export class BankAccount {
   constructor() {
