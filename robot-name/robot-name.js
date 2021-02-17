e8f51ad083329aa3796aed99533ae81f59050e2e
david <daverohrer@gmail.com>
2021-02-17 11:18:33 -0600

    finished robot

--- robot-name/robot-name.js
+++ robot-name/robot-name.js
@@ -10,99 +10,82 @@ const TOTAL_NUMBER_OF_NAMES =
     10 * // 0-9
     10; // 0-9
 
-const POSSIBLE_DIGITS = [
-    26,
-    26,
-    10,
-    10,
-    10
-]
-
 export class Robot {
-    constructor() {
-        if(!Robot.initializedNameList){
-            Robot._generateNames();
-            Robot.initializedNameList = true;
-        }
-        
-       // do {
 
-        //} (while )
-        const randomIndex = randNum(TOTAL_NUMBER_OF_NAMES);
-        this._name = `${Robot.validNames[randomIndex]}`;
+    constructor() {
+        if (!Robot._initializedNameManagment) {
+            Robot._initializeNameManagment();
+        }
+        this.reset();
     }
 
     get name() {
         return this._name;
     }
 
+    reset() {
+        this._name = Robot._nameBank[Robot._nextAvailableName];
+        Robot._nextAvailableName++;
+    }
 
     // Unique name management
-    static validNames = [];
-    static usedNames = [];
-    static initializedNameList = false;
+    static _nameBank = [];
+    static _nextAvailableName = 0;
+    static _initializedNameManagment = false;
+
     static releaseNames() {
-        Robot.usedNames = [];
+        Robot._nextAvailableName = 0;
     };
-    static _generateNames(name) {
 
-        //
-        const characters = [[], [], [], [], []];
-        for (let i = 0; i < 26; i++) {
-            characters[0].push(String.fromCharCode(65 + i));
-            characters[1].push(String.fromCharCode(65 + i));
-            if (i < 10) {
-                characters[2].push(String.fromCharCode(48 + i));
-                characters[3].push(String.fromCharCode(48 + i));
-                characters[4].push(String.fromCharCode(48 + i));
-            }
-        }
+    static _initializeNameManagment() {
+        Robot._createNameBank();
+        Robot._shuffleNameBank();
+        Robot._initializedNameManagment = true;
+    }
 
-        // Create the list of all possible names from the
+    static _createNameBank() {
+        const validCharacters = Robot._generateValidCharacers();
         for (let d1 = 0; d1 < 26; d1++) {
             for (let d2 = 0; d2 < 26; d2++) {
                 for (let d3 = 0; d3 < 10; d3++) {
                     for (let d4 = 0; d4 < 10; d4++) {
                         for (let d5 = 0; d5 < 10; d5++) {
-                            const nameToAdd = `${characters[0][d1]}${characters[1][d2]}${characters[2][d3]}${characters[3][d4]}${characters[4][d5]}`
-                            Robot.validNames.push(nameToAdd);
+                            const nameToAdd = `${validCharacters[0][d1]}` +
+                                `${validCharacters[1][d2]}${validCharacters[2][d3]}` +
+                                `${validCharacters[3][d4]}${validCharacters[4][d5]}`;
+                            Robot._nameBank.push(nameToAdd);
                         }
                     }
                 }
             }
         }
-        // if (true) { //if we need to set up the bool array for the first time
-        //     for (let d1 = 0; d1 < 26; d1++) {
-        //         for (let d2 = 0; d2 < 26; d2++) {
-        //             for (let d3 = 0; d3 < 10; d3++) {
-        //                 for (let d4 = 0; d4 < 10; d4++) {
-        //                     for (let d5 = 0; d5 < 10; d5++) {
-                                
-        //                         Robot.usedNames.push(false);
-        //                     }
-        //                 }
-        //             }
-        //         }
-        //     }
-        // }
+    }
 
-        // Shuffle the names up really well
-        for (let i = 0; i < TOTAL_NUMBER_OF_NAMES * 2; i++) {
-            const swapA = randNum(Robot.validNames.length);
-            const swapB = randNum(Robot.validNames.length);
-            if (swapA !== swapB) {
-                const tempName = Robot.validNames[swapA];
-                Robot.validNames[swapA] = Robot.validNames[swapB];
-                Robot.validNames[swapB] = tempName;
+    static _generateValidCharacers() {
+        const validCharacters = [[], [], [], [], []];
+        for (let i = 0; i < 26; i++) {
+            validCharacters[0].push(String.fromCharCode(65 + i));
+            validCharacters[1].push(String.fromCharCode(65 + i));
+            if (i < 10) {
+                validCharacters[2].push(String.fromCharCode(48 + i));
+                validCharacters[3].push(String.fromCharCode(48 + i));
+                validCharacters[4].push(String.fromCharCode(48 + i));
             }
         }
-        console.log(Robot.validNames);
+        return validCharacters;
+    }
+
+    static _shuffleNameBank() {
+        for (let i = 0; i < TOTAL_NUMBER_OF_NAMES * 2; i++) {
+            const swapA = randNum(Robot._nameBank.length);
+            const swapB = randNum(Robot._nameBank.length);
+            const tempName = Robot._nameBank[swapA];
+            Robot._nameBank[swapA] = Robot._nameBank[swapB];
+            Robot._nameBank[swapB] = tempName;
+        }
     }
 }
 
 function randNum(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
-
-
-
