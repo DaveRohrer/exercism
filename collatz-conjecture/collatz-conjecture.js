d949653c31df6fff21ce2719bd7f91f1cc72e753
david <daverohrer@gmail.com>
2021-02-19 08:08:37 -0600

    collatz-conjecture completed

--- collatz-conjecture/collatz-conjecture.js
+++ collatz-conjecture/collatz-conjecture.js
@@ -3,6 +3,21 @@
 // convenience to get you started writing code faster.
 //
 
-export const steps = () => {
-  throw new Error('Remove this statement and implement this function');
+export const steps = (n) => {
+
+  if (n <= 0) {
+    throw new Error('Only positive numbers are allowed');
+  }
+  return collatz(n, 0);
 };
+
+function collatz(n, step) {
+  if (n === 1) {
+    return step;
+  }
+  else if (!(n % 2)) {
+    return collatz(n / 2, step + 1)
+  } else {
+    return collatz(n * 3 + 1, step + 1)
+  }
+}
--- collatz-conjecture/collatz-conjecture.spec.js
+++ collatz-conjecture/collatz-conjecture.spec.js
@@ -4,26 +4,29 @@ describe('steps()', () => {
   test('zero steps for one', () => {
     expect(steps(1)).toEqual(0);
   });
+  test('1 steps for 2', () => {
+    expect(steps(2)).toEqual(1);
+  });
 
-  xtest('divide if even', () => {
+  test('divide if even', () => {
     expect(steps(16)).toEqual(4);
   });
 
-  xtest('even and odd steps', () => {
+  test('even and odd steps', () => {
     expect(steps(12)).toEqual(9);
   });
 
-  xtest('large number of even and odd steps', () => {
+  test('large number of even and odd steps', () => {
     expect(steps(1000000)).toEqual(152);
   });
 
-  xtest('zero is an error', () => {
+  test('zero is an error', () => {
     expect(() => {
       steps(0);
     }).toThrow(new Error('Only positive numbers are allowed'));
   });
 
-  xtest('negative value is an error', () => {
+  test('negative value is an error', () => {
     expect(() => {
       steps(-15);
     }).toThrow(new Error('Only positive numbers are allowed'));
