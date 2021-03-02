80b745a212364d9021d5db8cf1c1554f98ca7eb7
david <daverohrer@gmail.com>
2021-03-02 11:14:23 -0600

    deleting old projects from this repo

--- tictactoe/index.js
+++
@@ -1,7 +0,0 @@
-const { initializeView } = require("./view");
-const { initializeModel } = require("./model");
-const { initializeController } = require("./controller");
-
-initializeView();
-initializeModel();
-initializeController();
