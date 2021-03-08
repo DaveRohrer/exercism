8f6f90e90273e5311534f7b442ed95ebcb1ed659
david <daverohrer@gmail.com>
2021-03-08 12:23:52 -0600

    Chapter 14 Exercise 1 finished

---
+++ house/house.js
@@ -0,0 +1,16 @@
+//
+// This is only a SKELETON file for the 'House' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+//const nouns;
+
+export class House {
+  static verse(index) {
+    return ["This is the house that Jack built."];
+  }
+
+  static verses() {
+    throw new Error("Remove this statement and implement this function");
+  }
+}
